ebdce27044815b471300f4cd7693ce67bb0ab6b1
nurgul <NurgulDev>
2025-11-21 18:50:34 +0600

    header

--- src/appPages/site/components/layout/header/header.tsx
+++ src/appPages/site/components/layout/header/header.tsx
@@ -1,44 +1,51 @@
 "use client";
 import React from "react";
 import style from "./Header.module.scss";
-import { useHeaderStore } from "@/stores/useHeaderStore";
 import logo from "../../../../../assets/image/logo.png";
 import Image from "next/image";
 import Link from "next/link";
+import IconRowhead from "@/assets/image/IconRowhead";
 
 const Links = [
   {
     name: "Главная",
     href: "/",
   },
-    {
+  {
     name: "О нас",
     href: "/aboutUs",
   },
-    {
+  {
     name: "Курсы",
     href: "/courses",
   },
-   {
+  {
     name: "Контакты",
     href: "/contacts",
   },
 ];
 function Header() {
-  const { isOpenProfileMenu, setIsOpenProfileMenu } = useHeaderStore();
 
   return (
     <header className={style.header}>
       <div className="container">
         <div className={style.content}>
           <div className={style.logo}>
-            <Image src={logo} alt="logo" width={79} height={58} />
+            <Image src={logo} alt="logo" />
           </div>
-         {Links.map((link, index) => (
-                    <Link key={index} href={link.href}>
-                      {link.name}
-                    </Link>
-                  ))}
+          <div className={style.rout}>
+            {Links.map((link, index) => (
+              <div className={style.routText} key={index}>
+                <Link className={style.link} key={index} href={link.href}>
+                  {link.name}
+                </Link>
+              </div>
+            ))}
+          </div>
+          <div className={style.boxButton}>
+            <button className={style.button1}>Войти</button>
+            <button className={style.button2}>Присоединяйся <IconRowhead/></button>
+          </div>
         </div>
       </div>
     </header>
