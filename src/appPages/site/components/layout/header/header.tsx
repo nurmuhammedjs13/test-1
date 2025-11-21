4567e62a970b0d1ef52f70567e479c5135cd2c45
nurgul <NurgulDev>
2025-11-21 19:17:16 +0600

    rout

--- src/appPages/site/components/layout/header/header.tsx
+++ src/appPages/site/components/layout/header/header.tsx
@@ -1,30 +1,26 @@
 "use client";
+
 import React from "react";
+import { useRouter } from "next/navigation";
 import style from "./Header.module.scss";
-import logo from "../../../../../assets/image/logo.png";
 import Image from "next/image";
 import Link from "next/link";
+import logo from "../../../../../assets/image/logo.png";
 import IconRowhead from "@/assets/image/IconRowhead";
 
 const Links = [
-  {
-    name: "Главная",
-    href: "/",
-  },
-  {
-    name: "О нас",
-    href: "/aboutUs",
-  },
-  {
-    name: "Курсы",
-    href: "/courses",
-  },
-  {
-    name: "Контакты",
-    href: "/contacts",
-  },
+  { name: "Главная", href: "/home" },
+  { name: "О нас", href: "/aboutUs" },
+  { name: "Курсы", href: "/courses" },
+  { name: "Контакты", href: "/contacts" },
 ];
-function Header() {
+
+const Header: React.FC = () => {
+  const router = useRouter();
+
+  const handleLogin = () => {
+    router.push("/login");
+  };
 
   return (
     <header className={style.header}>
@@ -36,20 +32,24 @@ function Header() {
           <div className={style.rout}>
             {Links.map((link, index) => (
               <div className={style.routText} key={index}>
-                <Link className={style.link} key={index} href={link.href}>
+                <Link className={style.link} href={link.href}>
                   {link.name}
                 </Link>
               </div>
             ))}
           </div>
           <div className={style.boxButton}>
-            <button className={style.button1}>Войти</button>
-            <button className={style.button2}>Присоединяйся <IconRowhead/></button>
+            <button className={style.button1} onClick={handleLogin}>
+              Войти
+            </button>
+            <button className={style.button2}>
+              Присоединяйся <IconRowhead />
+            </button>
           </div>
         </div>
       </div>
     </header>
   );
-}
+};
 
 export default Header;
---
+++ src/appPages/site/components/pages/contactsSection/heroContacts/HeroContacts.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeroContacts() {
+  return (
+    <div>HeroContacts</div>
+  )
+}
+
+export default HeroContacts
